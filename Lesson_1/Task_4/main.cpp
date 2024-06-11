ab456d93dcab3fdda44df5a4d350ad45a6cab595
Miki <travelingspaceworker90@gmail.com>
2024-06-11 19:33:13 +0300

    Fixed 'bloat' in code

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -8,25 +8,22 @@ int main()
     float nFirst=0, nSecond=0;
 
     cout<<"This is a simple calculator. It can do anything simple with 2 given numbers. Firstly enter operator: +, -, *, /: "; //ask question
-    cin>>operat;                                                                      //operator input scan
-
-    cout<<"Enter first number: ";                                                     //request first number to work with
-    cin>>nFirst;
-    cout<<"Enter second number: ";                                                    //request first number to work with
-    cin>>nSecond;
+    cin>>operat;                                                                       //operator input scan
+    cout<<"Enter first and second number: ";                                           //request first number to work with
+    cin>>nFirst>>nSecond;
 
     cout<<"Here is ur result:"<<endl;
 
     switch(operat)
     {
     case '+':
-        cout<<nFirst<<' '<<operat<<' '<<nSecond<<" = "<<nFirst+nSecond;               //do addition if used specified operator
+        cout<<nFirst+nSecond;                                                         //do addition if used specified operator
         break;
     case '-':
-        cout<<nFirst<<' '<<operat<<' '<<nSecond<<" = "<<nFirst-nSecond;               //do subtraction if used specified operator
+        cout<<nFirst-nSecond;                                                         //do subtraction if used specified operator
         break;
     case '*':
-        cout<<nFirst<<' '<<operat<<' '<<nSecond<<" = "<<nFirst*nSecond;               //do multiplication if used specified operator
+        cout<<nFirst*nSecond;                                                         //do multiplication if used specified operator
         break;
     case '/':
         if(nSecond==0)
@@ -36,11 +33,11 @@ int main()
         }
         else
         {
-            cout<<nFirst<<' '<<operat<<' '<<nSecond<<" = "<<nFirst/nSecond;           //do division if second number non 0 and used specified operator
+            cout<<nFirst/nSecond;                                                     //do division if second number non 0 and used specified operator
         }
         break;
     default:
-        cout<<"Error! operator is not correct";                                       // If the operator is other than requested error message is shown.
+        cout<<"Error! Operator is not correct.";                                      // If the operator is other than requested error message is shown.
         break;
     }
 }
--- Lesson_1/Task_6/main.cpp
+++ Lesson_1/Task_6/main.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main()
 {
-    int size = 0;
-    int height = 0, width = 0;
+    int size=0;
+    int height=0, width=0;
 
-    std::cout<<"I will draw Christmas tree  4u."<<'\n'<<"The bigger the number, the bigger the tree will be."<<endl; //intro
+    cout<<"I will draw Christmas tree  4u."<<'\n'<<"The bigger the number, the bigger the tree will be."<<endl; //intro
     cin>>size;                                                      //scan the size value
 
-    for(width = 1; width <= size; width++)                          //the first loop works as long as [size] is less or equal to half of the triangle width
+    for(width=1;width<=size;width++)                                //the first loop works as long as [size] is less or equal to half of the triangle width
     {                                                               //and since the [size] of the triangle matches the number of the central column (its center) it can use the value of the [size] entered
-        for(height = 1; height <= (size + width); height++)         //second loop runs as long as [height] is less or equal to [size] plus the value of [width]
+        for(height=1;height<=(size+width);height++)                 //second loop runs as long as [height] is less or equal to [size] plus the value of [width]
         {
-            if(height <= (size - width + 1))                        //if [height] is less than or equal to [size] minus [width] plus 1.
+            if(height<=(size-width+1))                              //if [height] is less than or equal to [size] minus [width] plus 1.
                 putchar(' ');                                       //the unit [1] specifies the offset of the entire triangle from the left border of the screen. if the condition is met, type a [ ], otherwise type an [*].
             else
                 putchar('*');
@@ -22,4 +22,4 @@ int main()
         putchar('\n');
     }
 }
-//for some strane reson programm brakes when size>89(90 also brake)
+//for some strange reason program brakes when size>89(90 also brake)
