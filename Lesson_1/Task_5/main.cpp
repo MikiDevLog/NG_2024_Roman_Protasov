027879d51874ca52ca671d78fdd5439af7baa30b
Roma <travelingspaceworker90@gmail.com>
2024-06-12 18:12:05 +0000

    moved cout modification code segnent

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 int main()
 {
+    cout.setf(ios_base::fixed,ios_base::floatfield);
+    cout.precision(2);
+
     double coefA=0, coefB=0, coefC=0, Discr=0, Root1=0, Root2=0;
 
     cout<<"Here is simple quadratic equation calculator."<<'\n';                                          //intro
     cout<<"Enter coefficient a, b and c:"<<'\n';                                                          //ask+read coefficients
     cin>>coefA>>coefB>>coefC;
-    cout.setf(ios_base::fixed,ios_base::floatfield);
-    cout.precision(2);
     Discr=(coefB*coefB)-(4*coefA*coefC);
     if (Discr > 0)                                                                                        //if D > 0 prossed like normal+show all 2 possible roots
     {
